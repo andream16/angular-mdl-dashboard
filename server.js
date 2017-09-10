015686a1c0c36be3ee8cccd10ea364f731101978
andream16 <andrea.medda.uni@gmail.com>
2017-09-10 22:38:17 +0200

    .

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + 'dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
 
